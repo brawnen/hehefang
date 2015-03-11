756e7b1dd941a34dbb6e97246c334dec824ae8b9
xuzunyuan <xuzunyuan@yiwang.com>
2015-03-11 18:20:22 +0800

    v

--- src/main/java/com/afd/seller/controller/WsController.java
+++ src/main/java/com/afd/seller/controller/WsController.java
@@ -7,8 +7,19 @@ package com.afd.seller.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.afd.constants.product.ProductConstants.BrandShow$Status;
+import com.afd.model.seller.Seller;
+import com.afd.seller.util.LoginUtils;
+import com.afd.seller.util.LoginUtils.LoginInfo;
+import com.afd.service.order.IOrderService;
+import com.afd.service.order.IRetOrderService;
+import com.afd.service.product.IBrandShowService;
+import com.afd.service.product.IProductService;
+import com.afd.service.seller.ISellerService;
 
 /**
  * 卖家主工作台
@@ -18,9 +29,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class WsController {
+	@Autowired
+	ISellerService sellerService;
+
+	@Autowired
+	IProductService productService;
+
+	@Autowired
+	IOrderService orderService;
+
+	@Autowired
+	IRetOrderService retOrderService;
+
+	@Autowired
+	IBrandShowService brandShowService;
+
 	@RequestMapping("/ws/summary")
 	public String wsSummary(HttpServletRequest request,
 			HttpServletResponse response) {
+
+		LoginInfo loginInfo = LoginUtils.getLoginInfo(request);
+
+		// 卖家信息
+		Seller seller = sellerService.getSellerById(loginInfo.getSellerId());
+		request.setAttribute("seller", seller);
+
+		// 商品数量
+		request.setAttribute("productCount", productService
+				.getValidProductCountOfSeller(loginInfo.getSellerId()));
+
+		// 待处理订单数量
+		request.setAttribute("toBeProcessOrderCount", orderService
+				.getToBeProcessOrderCountOfSeller(loginInfo.getSellerId()));
+
+		// 待处理退货单数量
+		request.setAttribute("toBeProcessRetOrderCount", retOrderService
+				.getToBeProcessRetOrderCountOfSeller(loginInfo.getSellerId()));
+
+		// 专场数量
+		request.setAttribute(
+				"auditingBrandShowCount",
+				brandShowService.getBrandShowCountOfSellerByStatus(
+						loginInfo.getSellerId(), BrandShow$Status.WAIT_AUDIT));
+
+		request.setAttribute(
+				"rejectedBrandShowCount",
+				brandShowService.getBrandShowCountOfSellerByStatus(
+						loginInfo.getSellerId(), BrandShow$Status.REJECTED));
+
+		request.setAttribute(
+				"onlineBrandShowCount",
+				brandShowService.getBrandShowCountOfSellerByStatus(
+						loginInfo.getSellerId(), BrandShow$Status.ONLINE));
 
 		return "/ws/summary";
 	}
